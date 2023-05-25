de4b72a849cc30b87b9e8708bb7f08cb456aa1da
ammar almansor <ammaralmansor4@gmail.com>
2023-05-26 00:25:43 +0300

    add models

--- library/apps/authlib/models.py
+++ library/apps/authlib/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from django.conf import settings
+
+# class Client (AbstractUser):
+#     name = models.CharField(max_length=100)
+#     email = models.EmailField()
+#     books_borrowed = models.ManyToManyField(Book , blank = True)
+
+
+class Client (models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL ,
+        default=1,
+        on_delete=models.CASCADE,
+        related_name='client_user'
+    )
+
+    def __str__(self):
+        return self.user.username
--- library/apps/book/models.py
+++ library/apps/book/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from apps.authlib.models import Client
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    auther = models.CharField(max_length=100)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
 
-# Create your models here.
+    borrowing_price = models.DecimalField(
+        max_digits=10 , 
+        decimal_places=2
+    )
+    
+    quantity = models.PositiveBigIntegerField()
+
+    def __str__ (self):
+        return self.title
+    
+class BorrowedBook (models.Model):
+    client = models.ForeignKey(
+        Client,
+        on_delete= models.CASCADE,
+        related_name= "borrowedbook_set"
+    )
